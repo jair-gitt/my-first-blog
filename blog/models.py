000b1398d55053084ef91b94416f0067ef0eb069
jair-gitt <jairastivia@gmail.com>
2020-01-10 08:21:48 -0600

    descripción

--- blog/models.py
+++ blog/models.py
@@ -16,4 +16,9 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.title, self.text
+class Name(models.Model):
+    nombre = models.CharField(max_length=30)
+    edad = models.CharField(max_length=12)
+    def __str__(self):
+        return self.nombre
